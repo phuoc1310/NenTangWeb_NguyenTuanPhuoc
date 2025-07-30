8f15617ec622659dff9df98f9f5cfd83cc248c8b
phuoc1310 <165747125+phuoc1310@users.noreply.github.com>
2025-07-30 23:15:30 +0700

    Add files via upload

---
+++ DE_2/data.js
@@ -0,0 +1,23 @@
+const transactions = [
+  {
+    id: 1,
+    customer: "Võ Hoài An",
+    employee: "Nguyễn Văn B",
+    amount: "250 000",
+    date: "2024-06-06T09:00"
+  },
+  {
+    id: 2,
+    customer: "Hoàng Thị Thắng",
+    employee: "Nguyễn Văn A",
+    amount: "500 000",
+    date: "2024-06-07T10:30"
+  },
+    {
+    id: 13,
+    customer: "Trần Minh Tuấn",
+    employee: "Nguyễn Văn A",
+    amount: "500 000",
+    date: "2024-06-07T10:30"
+  }
+];
